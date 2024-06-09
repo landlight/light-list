4e439c45b28417f050729f5829e67d1974e35e26
Chit Khine (Light) <light@townbase.com>
2024-06-09 20:28:42 +0700

    :sparkles: add sign up

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,12 +6,15 @@ import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
 
 export default function LoginPage() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [isRegister, setIsRegister] = useState(false);
 
   const handleLogin = async () => {
     const { error } = await supabase.auth.signInWithPassword({
@@ -25,9 +28,27 @@ export default function LoginPage() {
     }
   };
 
+  const handleRegister = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      if (data) {
+        setSuccess(
+          "Successfully registered! Please check your email for verification!"
+        );
+      }
+      router.push("/todo");
+    }
+  };
+
   const handleKeyPress = (event: any) => {
     if (event.key === "Enter") {
-      handleLogin();
+      isRegister ? handleRegister() : handleLogin();
     }
   };
 
@@ -42,7 +63,7 @@ export default function LoginPage() {
         }}
       >
         <Typography component="h1" variant="h5">
-          Welcome from Light List
+          Welcome to Light List
         </Typography>
         <Box sx={{ mt: 1 }}>
           <TextField
@@ -56,7 +77,7 @@ export default function LoginPage() {
             autoFocus
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            onKeyPress={handleKeyPress}
+            onKeyDown={handleKeyPress}
           />
           <TextField
             margin="normal"
@@ -76,16 +97,32 @@ export default function LoginPage() {
               {error}
             </Typography>
           )}
+          {success && (
+            <Typography color="green" variant="body2">
+              {success}
+            </Typography>
+          )}
           <Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
             sx={{ mt: 3, mb: 2 }}
-            onClick={handleLogin}
+            onClick={isRegister ? handleRegister : handleLogin}
           >
-            Login
+            {isRegister ? "Register" : "Login"}
           </Button>
+          <Link
+            href="#"
+            color="inherit"
+            onClick={() => setIsRegister(!isRegister)}
+            variant="body2"
+            sx={{ color: "black" }}
+          >
+            {isRegister
+              ? "Already have an account? Login"
+              : "Don't have an account? Register"}
+          </Link>
         </Box>
       </Box>
     </Container>
